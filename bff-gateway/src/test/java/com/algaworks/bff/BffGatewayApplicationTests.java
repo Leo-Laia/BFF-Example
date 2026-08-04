cb041076d29f24acc6b9450334c36924872ee84c
Leonardo Laia Ribeiro <leonardolribo@gmail.com>
2026-08-04 20:52:37 -0300

    Estrutura build Gradle multi-modulo (#2)
    Co-authored-by: Leonardo Laia Ribeiro <182001151+Leo-Laia@users.noreply.github.com>

---
+++ bff-gateway/src/test/java/com/algaworks/bff/BffGatewayApplicationTests.java
@@ -0,0 +1,12 @@
+package com.algaworks.bff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffGatewayApplicationTests {
+
+    @Test
+    void deveCarregarContextoDaAplicacao() {
+    }
+}
