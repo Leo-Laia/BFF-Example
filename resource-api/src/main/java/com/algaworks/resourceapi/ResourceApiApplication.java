1610ca8f292d65ca3af8253ac47ea476e41fd5ee
Leonardo Laia Ribeiro <182001151+Leo-Laia@users.noreply.github.com>
2026-08-04 20:46:53 -0300

    Estrutura build Gradle multi-modulo

---
+++ resource-api/src/main/java/com/algaworks/resourceapi/ResourceApiApplication.java
@@ -0,0 +1,12 @@
+package com.algaworks.resourceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResourceApiApplication.class, args);
+    }
+}
---
+++ resource-api/src/test/java/com/algaworks/resourceapi/ResourceApiApplicationTests.java
@@ -0,0 +1,12 @@
+package com.algaworks.resourceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceApiApplicationTests {
+
+    @Test
+    void deveCarregarContextoDaAplicacao() {
+    }
+}
